0566f07568001d82b81bc512788864013f918a5f
Gerashenko Vladimir <gera8479@gmail.com>
2018-08-15 12:29:17 +0300

    iteration 2 (feature: added User.create test)

---
+++ test/functional/models/user.spec.js
@@ -0,0 +1,15 @@
+const {test, trait} = use('Test/Suite')('User')
+const User = use('App/Models/User')
+
+trait('DatabaseTransactions')
+
+test('User.create', async ({assert}) => {
+  await User.create({
+    firstName: 'john',
+    lastName: 'Doe',
+    email: 'john_doe@example.com',
+    password: '12345678'
+  })
+
+  assert.equal(await User.getCount(), 1)
+})
--- test/unit/lint/lint.spec.js
+++ test/unit/lint/lint.spec.js
@@ -1,30 +1,17 @@
+/* eslint-disable no-unused-expressions */
 'use strict'
 
 const { test } = use('Test/Suite')('Lint')
 const { exec } = require('child_process')
-const expect = require('chai').expect
+const {expect} = require('chai')
 
-test('standard lint', async ({assert}) => {
+test('standard lint', async () => {
   const promise = new Promise((resolve, reject) => {
-    exec('node ./node_modules/standard/bin/cmd.js', async (_, stdout, stderr) => {
-
+    exec('node ./node_modules/standard/bin/cmd.js', async (_, stdout, __) => {
       resolve(stdout)
-      // if (err) {
-      //   console.log(err)
-      //   console.log(`stdout: ${stdout}`)
-      //   console.log(`stderr: ${stderr}`)
-      //   node couldn't execute the command
-      // return
-      // }
-      // assert.equal(stderr, 123123)
-      // the *entire* stdout and stderr (buffered)
-      // console.log(`stdout: ${stdout}`)
-      // console.log(`stderr: ${stderr}`)
     })
   })
-  const result = await promise
-  console.log(expect)
 
-  // expect(result)
+  const result = await promise
   expect(result).to.be.empty
 })
